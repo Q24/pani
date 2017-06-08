0d2aa94dc8717f2677c0b681b252e5622a58c654
Paul Klos <paul.klos@qnh.nl>
2017-06-08 10:20:59 +0200

    Remove unnecessary dos2unix

--- installers/locale/provision.sh
+++ installers/locale/provision.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 echo "Setting system locale"
-dos2unix /installers/locale/locale.sh
-cp /installers/locale/locale.sh /etc/default/locale
+cp $1/locale.sh /etc/default/locale
 locale-gen en_US.UTF-8
 timedatectl set-timezone Europe/Amsterdam
 echo ""
